466d0b5e9eb7c6fc7efde340a11525d1d4c4063f
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-01-19 00:27:17 -0500

    Sending Data with post request

--- src/http.js
+++ src/http.js
@@ -5,6 +5,22 @@ export async function fetchAvailablePlaces(){
 
         throw new Error ('Failed t fetch places');
     }
-    
+
     return resData.places;
+}
+export async function updateUserPlaces(places){
+ const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({places: places}),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+ });
+ const resData = await response.json();
+
+ if(!response.ok){
+     throw new Error('Failed to update user data.');
+
+ }
+ return resData.message;
 }
