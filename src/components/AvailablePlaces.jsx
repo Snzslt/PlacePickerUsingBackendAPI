ff90ffa164033ab7f28e417e127bd7b5886e5f19
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-01-19 15:28:25 -0500

    Using Custom Hook in multiple Components

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,38 +1,38 @@
 import Places from './Places.jsx';
-import {useState, useEffect} from 'react';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
 
-export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
-  useEffect(() =>{
-    async function fetchPlaces(){
-      setIsFetching(true);
-      try{
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-            );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-       
-      } catch(error){
-       setError({
-         message: error.message || 'Could not fetch places, please try again later.'
+ async function fetchSortedPlaces(){
+   const places = await fetchAvailablePlaces();
+   return new Promise((resolve) => {
+     navigator.geolocation.getCurrentPosition((position) => {
+       const sortedPlaces = sortPlacesByDistance(
+         places,
+         position.coords.latitude,
+         position.coords.longitude
+         );
+         resolve(sortedPlaces);
        });
-       setIsFetching(false);
-      }
-    }
- fetchPlaces();
-}, []);
+   });
+ }
+  
+export default function AvailablePlaces({ onSelectPlace }) {
+ 
+
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces
+    
+  } =
+  useFetch(fetchSortedPlaces , []);
+
+
+
+     
+      
 
 if(error){
   return <Error title="An error occurred!" message = {error.message}/>
